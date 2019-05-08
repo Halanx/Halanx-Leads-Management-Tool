ec8942d9d6cdd0477a55faed0ad1dcff0e94efb8
nikhilkumarsingh <nikhilksingh97@gmail.com>
2019-05-08 15:26:15 +0530

    improved ajax_form_submit

--- static_dev/js/ajax_utils.js
+++ static_dev/js/ajax_utils.js
@@ -1,6 +1,19 @@
-function ajax_form_submit(form_id, success_callback=function(){}, success_result_alert=true) {
+function ajax_form_submit(form_id, options={}) {
+
+    let confirmation = options['confirmation'] || null;
+    let success_result_alert = options['success_result_alert'] || true;
+    let reload_after_submit = options['reload_after_submit'] || false;
+    let success_callback = options['success_callback'] || function(){};
+
     $(form_id).submit(function (e) {
         e.preventDefault();
+
+        if(confirmation !== null) {
+            if (!confirm(confirmation)) {
+                return;
+            }
+        }
+
         let form = $(this);
         let url = form.attr('action');
 
@@ -9,15 +22,24 @@ function ajax_form_submit(form_id, success_callback=function(){}, success_result
             url: url,
             data: form.serialize(),
             success: function (data) {
-                    if(success_result_alert) {
-                        alert(data['detail']);
-                    }
-                    success_callback();
+                if(success_result_alert) {
+                    alert(data['detail']);
                 }
+                if(reload_after_submit) {
+                    location.reload();
+                }
+                success_callback();
+            },
+            error: function( jqXhr, textStatus, errorThrown ){
+                console.log(errorThrown);
+                alert("Some error occurred.");
+                if(reload_after_submit) {
+                    location.reload();
+                }
+            }
         });
     });
 }
-
 $(document).ajaxSend(function () {
     $('#ajaxLoad').addClass('show');
 });
