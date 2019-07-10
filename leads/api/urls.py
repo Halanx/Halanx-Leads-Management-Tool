8e61d690c57d3faf238e7d798cfb2d52752d6af0
nimish <nimish4july1998@gmail.com>
2019-07-10 12:49:23 +0530

    added bulk lead from tenant,owner referrals via csv

--- leads/api/urls.py
+++ leads/api/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from leads.api import views
 
 urlpatterns = [
-    url('tenant/referrals', views.tenant_referral_lead_create_view),
-    url('owner/referrals', views.owner_referral_lead_create_view),
+    url('tenant/referrals/$', views.tenant_referral_lead_create_view),
+    url('owner/referrals/$', views.owner_referral_lead_create_view),
+    url('tenant/referrals/bulk/$', views.tenant_csv_referral_lead_create_view),
+    url('owner/referrals/bulk/$', views.owner_csv_referral_lead_create_view),
 ]
--- leads/api/views.py
+++ leads/api/views.py
@@ -14,7 +14,6 @@ def tenant_referral_lead_create_view(request):
     """It is used to create a lead for each tenant referral"""
     if request.user.is_superuser:
         data = request.data
-        print(data, 'hi')
         try:
             TenantLead.objects.create(**data)
             response_json = {STATUS: SUCCESS}
@@ -41,3 +40,42 @@ def owner_referral_lead_create_view(request):
             return JsonResponse(response_json, status=400)
 
 
+@api_view(('POST',))
+@authentication_classes((BasicAuthentication,))
+@permission_classes((IsAdminUser,))
+def tenant_csv_referral_lead_create_view(request):
+    """It is used to create leads for all referrals in csv"""
+    if request.user.is_superuser:
+        status_response_list = []
+        data_list = request.data  # List of referrals
+        for data in data_list:
+            try:
+                TenantLead.objects.create(**data)
+                response_json = {STATUS: SUCCESS}
+                status_response_list.append(response_json)
+            except Exception as E:
+                response_json = {STATUS: ERROR, 'message': str(E)}
+                status_response_list.append(response_json)
+
+        return JsonResponse(status_response_list ,safe=False)
+
+
+@api_view(('POST',))
+@authentication_classes((BasicAuthentication,))
+@permission_classes((IsAdminUser,))
+def owner_csv_referral_lead_create_view(request):
+    """It is used to create leads for all referrals in csv"""
+    if request.user.is_superuser:
+        status_response_list = []
+        data_list = request.data  # List of referrals
+        for data in data_list:
+            try:
+                HouseOwnerLead.objects.create(**data)
+                response_json = {STATUS: SUCCESS}
+                status_response_list.append(response_json)
+            except Exception as E:
+                response_json = {STATUS: ERROR}
+                status_response_list.append(response_json)
+
+        print(status_response_list)
+        return JsonResponse(status_response_list, safe=False)
