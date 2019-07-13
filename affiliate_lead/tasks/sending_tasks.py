1b51c671611f3d5e43276dc865849c8a4fa4b58e
nimish <nimish4july1998@gmail.com>
2019-07-13 13:14:05 +0530

    Added house owner lead activity status updatae view

--- affiliate_lead/tasks/sending_tasks.py
+++ affiliate_lead/tasks/sending_tasks.py
@@ -3,10 +3,10 @@ import json
 import requests
 from decouple import config
 
-from affiliate_lead.utils import TENANT_LEAD_REFERRAL_UPDATE_URL
-from leads.api.serializers import TenantLeadActivitySerializer
+from affiliate_lead.utils import TENANT_LEAD_REFERRAL_UPDATE_URL, HOUSE_OWNER_LEAD_REFERRAL_UPDATE_URL
+from leads.api.serializers import TenantLeadActivitySerializer, HouseOwnerLeadActivitySerializer
 from leads.utils import DATA, TASK_TYPE, UPDATE_TENANT_LEAD_ACTIVITY_STATUS, STATUS_DISQUALIFIED, CANCELLED, PENDING, \
-    STATUS_CONVERTED, STATUS_VISIT_SCHEDULED, METADATA
+    STATUS_CONVERTED, STATUS_VISIT_SCHEDULED, METADATA, UPDATE_HOUSE_OWNER_LEAD_ACTIVITY_STATUS
 from utility.logging_utils import sentry_debug_logger
 from utility.response_utils import STATUS, SUCCESS
 
@@ -57,3 +57,31 @@ def update_tenant_lead_activity_status_in_affiliate_tool(tenant_lead_activity):
             if req.json()[STATUS] == SUCCESS:
                 tenant_lead_activity.acknowledged_by_affiliate = True
                 tenant_lead_activity.save()
+
+
+def update_owner_lead_activity_status_in_affiliate_tool(owner_lead_activity):
+    request_data = {
+        TASK_TYPE: UPDATE_HOUSE_OWNER_LEAD_ACTIVITY_STATUS,
+        DATA: HouseOwnerLeadActivitySerializer(owner_lead_activity).data,
+        METADATA: {'referral_status': get_appropriate_status_of_lead_activity_for_affiliate_tool(
+            lead_activity=owner_lead_activity)}
+    }
+
+    owner_referral_id = None
+    if owner_lead_activity.lead.referral_id:
+        owner_referral_id = owner_lead_activity.lead.referral_id
+
+    if owner_referral_id:
+        req = requests.patch(HOUSE_OWNER_LEAD_REFERRAL_UPDATE_URL.format(**{'pk': owner_referral_id}),
+                             data=json.dumps(request_data),
+                             headers={'Content-type': 'application/json'},
+                             timeout=10,
+                             auth=(config('AFFILIATE_TOOL_ADMIN_USERNAME'), config('AFFILIATE_TOOL_ADMIN_PASSWORD')))
+
+        print(req.status_code)
+        print(str(req.content))
+
+        if req.status_code == 200:
+            if req.json()[STATUS] == SUCCESS:
+                owner_lead_activity.acknowledged_by_affiliate = True
+                owner_lead_activity.save()
--- leads/api/serializers.py
+++ leads/api/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from leads.models import TenantLeadActivity
+from leads.models import TenantLeadActivity, HouseOwnerLeadActivity
 
 
 class TenantLeadActivitySerializer(serializers.ModelSerializer):
     class Meta:
         model = TenantLeadActivity
+        fields = '__all__'
+
+
+class HouseOwnerLeadActivitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = HouseOwnerLeadActivity
         fields = '__all__'
