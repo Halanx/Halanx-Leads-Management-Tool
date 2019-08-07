c7f352f4bdef5b4e377084e2c72abd2b7e78c9b4
nimish <nimish4july1998@gmail.com>
2019-08-07 17:20:16 +0530

    added celery

--- HalanxLeadManagementTool/settings/base.py
+++ HalanxLeadManagementTool/settings/base.py
@@ -88,3 +88,12 @@ USE_TZ = True
 SITE_ID = 1
 
 AFFILIATE_DB = 'affiliate'
+
+
+# CELERY STUFF
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'UTC'
